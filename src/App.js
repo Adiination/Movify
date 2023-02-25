cf53ec1612470d4d7c4d8e8489fedcc96054329d
Adiination <Aditya.sasaram11@gmail.com>
2023-02-26 01:46:36 +0530

    making the header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+       <img alt="Movify logo" src="./Assets/mofify.jpg"></img>
+      <span><h1>MOVIFY</h1></span>
+      <span><p>Home</p> </span>
+     <span><p>My list</p> </span>
+     <span> <p>services</p> </span>
+      
+     
     </div>
   );
 }
 
 export default App;
+
+
+
