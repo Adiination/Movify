9f630b7398427d62ce77a5272c10ba5977d139a5
Adiination <Aditya.sasaram11@gmail.com>
2023-03-02 08:48:26 +0530

    making the header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 function App() {
   return (
     <div className="header">
+      <div className='headerLeft'>
        <img alt="Movify logo" src="./Assets/mofify.jpg"></img>
       <span><h1>MOVIFY</h1></span>
       <span><p>Home</p> </span>
      <span><p>My list</p> </span>
      <span> <p>services</p> </span>
+     </div>
       
      
     </div>
